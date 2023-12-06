145f9652e19f253a0352e6be3454a6198c4d4325
Till Seeberger <seebergertill@gmail.com>
2023-12-06 21:42:18 +0100

    Fix typo

--- Formula/hmq-tui.rb
+++ Formula/hmq-tui.rb
@@ -1,5 +1,5 @@
 class HmqTui < Formula
-  desc "Unoffical TUI for interacting with the HiveMQ REST API"
+  desc "Unofficial TUI for interacting with the HiveMQ REST API"
   homepage "https://github.com/gitseti/hmq-tui"
   url "https://github.com/gitseti/hmq-tui/archive/refs/tags/0.0.1.tar.gz"
   sha256 "5788b6d5335055e8ae5b6719ce0b573106311eb8165941a22b07142e9da9c8c6"
