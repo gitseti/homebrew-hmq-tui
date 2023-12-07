3ef86a77876391169ddf026760f8a54252be5566
gitseti <till.seeberger@hivemq.com>
2023-12-07 22:58:03 +0100

    hmq-tui 0.1.0 (new-formula)

---
+++ Formula/hmq-tui.rb
@@ -0,0 +1,17 @@
+class HmqTui < Formula
+  desc "Unofficial TUI for interacting with the HiveMQ REST API"
+  homepage "https://github.com/gitseti/hmq-tui"
+  url "https://github.com/gitseti/hmq-tui/archive/0.1.0.tar.gz"
+  sha256 "122020b47e4a7ad9b2fa142705c64bd569ae0aaa3e50b05924c655ba2f34946c"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "hmq-tui " + version.to_s, shell_output("#{bin}/hmq --version")
+  end
+end
