bd186038773c8524b39c8f0788de44700aabfde7
gitseti <till.seeberger@hivemq.com>
2024-05-11 17:01:13 +0200

    hmq-tui 0.3.0
    Closes #4.
    
    Signed-off-by: Till Seeberger <seebergertill@gmail.com>

--- Formula/hmq-tui.rb
+++ Formula/hmq-tui.rb
@@ -1,8 +1,8 @@
 class HmqTui < Formula
   desc "Unofficial TUI for interacting with the HiveMQ REST API"
   homepage "https://github.com/gitseti/hmq-tui"
-  url "https://github.com/gitseti/hmq-tui/archive/refs/tags/0.2.0.tar.gz"
-  sha256 "abd69d3670777e006d5063d678bce4a355aae6c4fcdb1790714bc5e97c6d2306"
+  url "https://github.com/gitseti/hmq-tui/archive/refs/tags/0.3.0.tar.gz"
+  sha256 "7aeb9bec0896050c90b8a050f0d0190327906c05ad755aa3f13d9f9d11a827ad"
   license "MIT"
 
   bottle do
